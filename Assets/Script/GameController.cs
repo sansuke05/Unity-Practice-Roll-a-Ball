25ab0a6d97301993b069e30537aa16e39fe22929
Takanori Watanabe <takanori5064@gmail.com>
2018-06-05 01:00:33 +0900

    チュートリアル完了+機能追加

---
+++ Assets/Script/GameController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public UnityEngine.UI.Text scoreLabel;
+	public GameObject winnerLabelObject;
+	public GameObject player;
+	
+	void Update () {
+		int count = GameObject.FindGameObjectsWithTag("Item").Length;
+		scoreLabel.text = count.ToString();
+
+		if(count == 0){
+			// オブジェクトをアクティブにする
+			winnerLabelObject.SetActive(true);
+			// ボールの動きを止める
+			player.GetComponent<Rigidbody>().velocity = Vector3.zero;
+		}
+	}
+}
